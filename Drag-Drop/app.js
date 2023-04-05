75f019a2e484db043118fbbca956534fccd0fa3a
milley69 <69milley@gmail.com>
2023-04-05 11:18:47 +0500

    hehe

---
+++ Drag-Drop/app.js
@@ -0,0 +1,28 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', (e) => {
+  e.target.classList.add('hold')
+  setTimeout(() => {
+    e.target.classList.add('hide')
+  }, 0)
+})
+item.addEventListener('dragend', (e) => {
+  e.target.className = 'item'
+})
+
+for (const placeholder of placeholders) {
+  placeholder.addEventListener('dragover', (e) => {
+    e.preventDefault()
+  })
+  placeholder.addEventListener('dragenter', (e) => {
+    e.target.classList.add('hovered')
+  })
+  placeholder.addEventListener('dragleave', (e) => {
+    e.target.classList.remove('hovered')
+  })
+  placeholder.addEventListener('drop', (e) => {
+    e.target.classList.remove('hovered')
+    e.target.append(item)
+  })
+}
